6363c5a68a2cd237f701d1164093fd50457fa5ce
GNULinuxIsGreat <GNULinuxIsGreat@users.noreply.github.com>
2020-07-19 16:20:27 +0200

    rewrite push_colname push_rowname
    rewrite in a more idiomatic way:
    pub(crate) fn push_colname(buf: &mut String, mut col: ucell);
    pub(crate) fn push_rowname(buf: &mut String, mut row: ucell);
    
    add test for this two functions(compatibility with the old one)
    
    correction of the overflow bug #6

--- src/refs.rs
+++ src/refs.rs
@@ -711,51 +711,22 @@ pub(crate) fn parse_cellranges(
 }
 
 /// Appends the spreadsheet column name.
-pub(crate) fn push_colname(buf: &mut String, mut col: ucell) {
-    let mut i = 0;
-    let mut dbuf = [0u8; 7];
+pub(crate) fn push_colname(buf: &mut String, col: ucell) {
+    let mut col: u64 = col as u64 + 1;
+    let mut _buf = String::new();
 
-    col += 1;
-    while col > 0 {
-        dbuf[i] = (col % 26) as u8;
-        if dbuf[i] == 0 {
-            dbuf[i] = 25;
-            col = col / 26 - 1;
-        } else {
-            dbuf[i] -= 1;
-            col /= 26;
-        }
-
-        i += 1;
+    while col != 0 {
+        _buf.push(std::char::from_u32((col % 26) as u32 - 1 + 'A' as u32).unwrap());
+        col /= 26;
     }
 
-    // reverse order
-    let mut j = i;
-    while j > 0 {
-        buf.push((b'A' + dbuf[j - 1]) as char);
-        j -= 1;
-    }
+    buf.push_str(&_buf.chars().rev().collect::<String>());
 }
 
 /// Appends the spreadsheet row name
-pub(crate) fn push_rowname(buf: &mut String, mut row: ucell) {
-    let mut i = 0;
-    let mut dbuf = [0u8; 10];
-
-    row += 1;
-    while row > 0 {
-        dbuf[i] = (row % 10) as u8;
-        row /= 10;
-
-        i += 1;
-    }
-
-    // reverse order
-    let mut j = i;
-    while j > 0 {
-        buf.push((b'0' + dbuf[j - 1]) as char);
-        j -= 1;
-    }
+fn push_rowname(buf: &mut String, row: ucell) {
+    let row: u64 = row as u64 + 1;
+    buf.push_str(&row.to_string());
 }
 
 /// Appends the table-name
