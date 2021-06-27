5c7ed29132ed2bbf7320df578773499fc12bbb5b
stommy <thscharler@gmail.com>
2021-06-27 13:36:47 +0200

    Add f*ref functions for absolute rows/columns/both.

--- src/formula.rs
+++ src/formula.rs
@@ -13,13 +13,70 @@ pub fn fcellref(row: ucell, col: ucell) -> String {
 }
 
 /// Creates a cell-reference for use in formulas.
+/// Creates an absolute row reference.
+pub fn fcellrefr(row: ucell, col: ucell) -> String {
+    CellRef::local(row, col).absolute_row().to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
+/// Creates an absolute col reference.
+pub fn fcellrefc(row: ucell, col: ucell) -> String {
+    CellRef::local(row, col).absolute_col().to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
+/// Creates an absolute reference.
+pub fn fcellrefa(row: ucell, col: ucell) -> String {
+    CellRef::local(row, col).absolute().to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
 pub fn fcellref_table<S: Into<String>>(table: S, row: ucell, col: ucell) -> String {
     CellRef::remote(table, row, col).to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
+/// Creates an absolute row reference.
+pub fn fcellrefr_table<S: Into<String>>(table: S, row: ucell, col: ucell) -> String {
+    CellRef::remote(table, row, col).absolute_row().to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
+/// Creates an absolute col reference.
+pub fn fcellrefc_table<S: Into<String>>(table: S, row: ucell, col: ucell) -> String {
+    CellRef::remote(table, row, col).absolute_col().to_formula()
+}
+
+/// Creates a cell-reference for use in formulas.
+/// Creates an absolute reference.
+pub fn fcellrefa_table<S: Into<String>>(table: S, row: ucell, col: ucell) -> String {
+    CellRef::remote(table, row, col).absolute().to_formula()
 }
 
 /// Creates a cellrange-reference for use in formulas.
 pub fn frangeref(row: ucell, col: ucell, row_to: ucell, col_to: ucell) -> String {
     CellRange::local(row, col, row_to, col_to).to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefr(row: ucell, col: ucell, row_to: ucell, col_to: ucell) -> String {
+    CellRange::local(row, col, row_to, col_to)
+        .absolute_rows()
+        .to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefc(row: ucell, col: ucell, row_to: ucell, col_to: ucell) -> String {
+    CellRange::local(row, col, row_to, col_to)
+        .absolute_cols()
+        .to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefa(row: ucell, col: ucell, row_to: ucell, col_to: ucell) -> String {
+    CellRange::local(row, col, row_to, col_to)
+        .absolute()
+        .to_formula()
 }
 
 /// Creates a cellrange-reference for use in formulas.
@@ -31,4 +88,43 @@ pub fn frangeref_table<S: Into<String>>(
     col_to: ucell,
 ) -> String {
     CellRange::remote(table, row, col, row_to, col_to).to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefr_table<S: Into<String>>(
+    table: S,
+    row: ucell,
+    col: ucell,
+    row_to: ucell,
+    col_to: ucell,
+) -> String {
+    CellRange::remote(table, row, col, row_to, col_to)
+        .absolute_rows()
+        .to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefc_table<S: Into<String>>(
+    table: S,
+    row: ucell,
+    col: ucell,
+    row_to: ucell,
+    col_to: ucell,
+) -> String {
+    CellRange::remote(table, row, col, row_to, col_to)
+        .absolute_cols()
+        .to_formula()
+}
+
+/// Creates a cellrange-reference for use in formulas.
+pub fn frangerefa_table<S: Into<String>>(
+    table: S,
+    row: ucell,
+    col: ucell,
+    row_to: ucell,
+    col_to: ucell,
+) -> String {
+    CellRange::remote(table, row, col, row_to, col_to)
+        .absolute()
+        .to_formula()
 }
