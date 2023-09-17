d37cdaaf3100be53f7e9d60a393f4c7eac49c999
stommy <thscharler@gmail.com>
2023-09-17 09:33:08 +0200

    removed warnings

--- tests/test_write_read.rs
+++ tests/test_write_read.rs
@@ -160,7 +160,7 @@ fn test_write_write() -> Result<(), OdsError> {
     wb.push_sheet(sh);
 
     let v = Cursor::new(Vec::new());
-    let _ = write_ods_to(&mut wb, v)?;
+    write_ods_to(&mut wb, v)?;
 
     Ok(())
 }
