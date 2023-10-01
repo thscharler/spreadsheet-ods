b385b258f1b4980060c0bf72e054a9045736be8d
stommy <thscharler@gmail.com>
2023-10-01 14:02:26 +0200

    correct some unused tags.
    correct some wrong tags.

--- tests/test_fods.rs
+++ tests/test_fods.rs
@@ -3,10 +3,7 @@ use spreadsheet_ods::{read_fods, read_ods, write_fods, OdsError};
 #[test]
 fn read_write_fods() -> Result<(), OdsError> {
     let mut wb = read_ods("tests/orders.ods")?;
-
     write_fods(&mut wb, "test_out/orders.fods")?;
-
     let wb = read_fods("test_out/orders.fods")?;
-
     Ok(())
 }
